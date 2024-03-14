c230977f78beb726ef60799fa5a16b7b9a38b8cc
Davor Kušić <dkusic@foi.hr>
2024-03-14 13:17:27 +0100

    vacations

---
+++ Vacations/.history/test_20240229104442.php
@@ -0,0 +1,6 @@
+public function test()
+{
+$a = 1;
+$b = $a + 1;
+return $b;
+}
---
+++ Vacations/src/Entity/Role.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RoleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RoleRepository::class)]
+class Role
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $roleName = null;
+
+    #[ORM\Column]
+    private ?string $roleTag = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRoleName(): ?string
+    {
+        return $this->roleName;
+    }
+
+    public function setRoleName(string $roleName): static
+    {
+        $this->roleName = $roleName;
+
+        return $this;
+    }
+
+    public function getRoleTag(): ?string
+    {
+        return $this->roleTag;
+    }
+
+    public function setRoleTag(string $roleTag): static
+    {
+        $this->roleTag = $roleTag;
+
+        return $this;
+    }
+}
